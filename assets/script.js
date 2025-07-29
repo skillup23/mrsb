e5a748652128342d105b9a5e5084fba489acaca1
Robert Anisimov <skillup23@yandex.ru>
2025-07-29 18:00:51 +0300

    регионы v2

--- assets/script.js
+++ assets/script.js
@@ -50,3 +50,13 @@ document
         alert('Произошла ошибка при отправке');
       });
   });
+
+// +++++++++++ Скрытие и открытие блока Регионы при клике +++++++++
+const buttonRegion = document.querySelector('.regions_button');
+const buttonRegionArrow = document.querySelector('.regions_button-arrow');
+const blockRegion = document.querySelector('.regions');
+
+buttonRegion.addEventListener('click', function () {
+  blockRegion.classList.toggle('visible-block');
+  buttonRegionArrow.classList.toggle('rotate180');
+});

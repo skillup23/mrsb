696c3240f3e0942f0734077f88430db822e5741a
skillup23 <skillup23@yandex.ru>
2025-08-12 23:07:18 +0300

    услуги 5

--- assets/accordeon.js
+++ assets/accordeon.js
@@ -1,20 +1,20 @@
 // +++++++++++ Аккордеон +++++++++
-document.addEventListener('DOMContentLoaded', () => {
-  const headers = document.querySelectorAll('.accordion-header');
+document.addEventListener("DOMContentLoaded", () => {
+  const headers = document.querySelectorAll(".accordion-header");
 
   headers.forEach((header) => {
-    header.addEventListener('click', function () {
+    header.addEventListener("click", function () {
       // Удаляем активный класс у всех заголовков
-      headers.forEach((h) => h.classList.remove('active'));
+      // headers.forEach((h) => h.classList.remove('active'));
 
       const content = this.nextElementSibling;
 
       if (content.style.maxHeight) {
         content.style.maxHeight = null;
-        this.classList.remove('active');
+        this.classList.remove("active");
       } else {
-        content.style.maxHeight = content.scrollHeight + 'px';
-        this.classList.add('active');
+        content.style.maxHeight = content.scrollHeight + "px";
+        this.classList.add("active");
       }
     });
   });

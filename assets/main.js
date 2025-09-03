1acad25dd2e3e3023214afa293ee9629800a25ec
Robert Anisimov <skillup23@yandex.ru>
2025-09-03 13:57:10 +0300

    публикация 0.1

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,9 @@
 // +++++++++++ Скрытие и открытие блока Регионы при клике +++++++++
-const buttonRegion = document.querySelector('.regions_button');
+const buttonRegion1 = document.querySelector('.regions_button');
 const buttonRegionArrow = document.querySelector('.regions_button-arrow');
 const blockRegion = document.querySelector('.regions');
 
-buttonRegion.addEventListener('click', function () {
+buttonRegion1.addEventListener('click', function () {
   blockRegion.classList.toggle('visible-block');
   buttonRegionArrow.classList.toggle('rotate180');
 });
